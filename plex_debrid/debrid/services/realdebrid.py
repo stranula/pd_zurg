245150e859c2a7edae9e4a05f4a54df802a5d8e0
stranula <stranula@gmail.com>
2024-07-27 13:19:02 -0400

    Update realdebrid.py

--- plex_debrid/debrid/services/realdebrid.py
+++ plex_debrid/debrid/services/realdebrid.py
@@ -180,6 +180,7 @@ def download(element, stream=True, query='', force=False):
     # Extract data for CSV
     data = extract_element_data(element)
     cached = element.Releases
+    print("Cached: " + cached)
     if query == '':
         query = element.deviation()
     wanted = [query]
